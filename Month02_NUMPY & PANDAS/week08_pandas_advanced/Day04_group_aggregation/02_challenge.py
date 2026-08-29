76f5ba1593fb588704aaa76125b194cb5d39c4a0
CodeByOmi <omkarnarawade62@gmail.com>
2026-08-29 20:37:13 +0530

    week 08 updated

--- Month02_NUMPY & PANDAS/week08_pandas_advanced/Day04_group_aggregation/02_challenge.py
+++ Month02_NUMPY & PANDAS/week08_pandas_advanced/Day04_group_aggregation/02_challenge.py
@@ -1,3 +1,44 @@
 import pandas as pd 
 
 df = pd.read_csv("Month02_NUMPY & PANDAS/week07_pandas_foundation/data/gym_members.csv")
+
+result = df.groupby("Membership")["Name"].count()
+
+print(result.sort_values(ascending=False))
+
+result = df.groupby("City").agg({
+    "Name": "count",
+    "Age": "mean",
+    "Weight_kg": "mean"
+})
+
+print(result)
+
+result = df.groupby("Membership")["Weight_kg"].mean()
+
+print(result.sort_values(ascending=False))
+
+result = df.groupby(
+    ["Membership", "Gender"]
+)["Weight_kg"].mean()
+
+print(result)
+
+result = df.groupby("City").agg(
+    Member_Count=("Name", "count"),
+    Average_Weight=("Weight_kg", "mean")
+)
+
+result = result.reset_index()
+
+result = result.sort_values(
+    "Member_Count",
+    ascending=False
+)
+
+print(result)
+
+df.groupby("City").agg(
+    Member_Count=("Name", "count"),
+    Average_Weight=("Weight_kg", "mean")
+)
---
+++ Month02_NUMPY & PANDAS/week08_pandas_advanced/Day05_mastery_day/full_week.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("Month02_NUMPY & PANDAS/week07_pandas_foundation/data/gym_members.csv")
+
